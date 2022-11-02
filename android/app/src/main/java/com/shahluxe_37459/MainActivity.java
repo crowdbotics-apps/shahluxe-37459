0bada3f4faa0cebc90253cbb88aec157a5327885
Crowdbotics <team@crowdbotics.com>
2022-11-02 03:33:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shahluxe_37459/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shahluxe_37459;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shahluxe_37459";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shahluxe_37459",
+  url: "https://shahluxe_37459.botics.co",
+  api: "https://shahluxe_37459.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
